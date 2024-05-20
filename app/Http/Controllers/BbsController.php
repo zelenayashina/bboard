71969ab01b3893d6b2b1a36ffcbe2cd62b7dd391
zelenayashina <zelenayashina@gmail.com>
2024-05-20 11:37:43 +0300

    First commit

---
+++ app/Http/Controllers/BbsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Bb;
+
+class BbsController extends Controller {
+    public function index() {
+        $context = ['bbs' => Bb::latest()->get()];
+        return view('index', $context);
+        }
+    public function detail(Bb $bb) {
+        return view('detail', ['bb' => $bb]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use App\Http\Controllers\BbsController;
+
+Route::get('/', [BbsController::class, 'index'])->name('index');
+Route::get('/{bb}', [BbsController::class, 'detail'])->name('detail');
